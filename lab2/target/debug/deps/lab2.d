839424fb1996cdbbc159dcf8d29ead7b8c19295b
KostyantinSolod <solodkosta@gmail.com>
2022-12-18 20:22:50 +0300

    q

---
+++ lab2/target/debug/deps/lab2.d
@@ -0,0 +1,6 @@
+d:\os-labs-2022\lab2\target\debug\deps\lab2.exe: src\main.rs src\vga_buf.rs
+
+d:\os-labs-2022\lab2\target\debug\deps\lab2.d: src\main.rs src\vga_buf.rs
+
+src\main.rs:
+src\vga_buf.rs:
